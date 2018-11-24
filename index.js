b3df630eee1c7b367e1fb2dcb3866584f118c79f
Maharoz <mahruj66@gmail.com>
2018-11-24 13:40:01 +0600

    object literal

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('hello world');
+const circle = { 
+    radius :1 ,
+    location:{
+        x:1,
+        y:1
+    },
+    draw: function(){
+        console.log('draw');
+    }
+};
+
+//object literal syntax demo
+circle.draw(); //calling the draw method
