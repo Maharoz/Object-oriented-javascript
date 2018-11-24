899da46c4c7b525750e3ccb2f0d0e0555c18b154
Maharoz <mahruj66@gmail.com>
2018-11-24 14:34:42 +0600

    constructor implemented

--- index.js
+++ index.js
@@ -10,4 +10,19 @@ function createCircle(raduis){
 }
 
 const circle = createCircle(1);
-circle.draw(); 
+
+
+//constructor function
+
+function Circle(radius){
+    this.raduis = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
+}
+
+const another = new Circle(1)
+
+
+//there are two ways of creating object in js 
+// one is factory function and another one is constructor function
