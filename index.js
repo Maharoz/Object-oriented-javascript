4b1e0436102261ac7f31947f2060262329db7ed2
Maharoz <mahruj66@gmail.com>
2018-12-09 22:04:29 +0600

    enumurating properties

--- index.js
+++ index.js
@@ -1,7 +1,34 @@
-let obj = { value:10};
-
-function increase(obj){
-    obj.value++;
+function Circle(radius){
+    this.radius = radius;
+    this.draw = function(){
+        console.log('draw');
+    }
 }
-increase(obj);
-console.log(obj);
+
+const circle = new Circle(10);
+
+//printing the property of object
+for (let key in circle){
+    console.log(key);
+}
+
+//printing the property of object with value
+for (let key in circle){
+    console.log(key,circle[key]);
+}
+
+//printing the property of object with value using filtering
+for (let key in circle){
+    if(typeof circle[key]!== 'function'){
+        console.log(key,circle[key]);
+    } 
+}
+
+//printing the property of object with value using array format
+const keys = Object.keys(circle);
+console.log(keys);
+
+//checking anny property exists on a object or not
+if('radius' in circle){
+    console.log('circle has radius');
+}
