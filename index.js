7208717ab93eb05afef4dc86ada14f750fad6c3f
Maharoz <mahruj66@gmail.com>
2018-12-09 22:21:02 +0600

    private properties and methods implemented

--- index.js
+++ index.js
@@ -1,34 +1,20 @@
 function Circle(radius){
+
+    let color = 'red'; //local variable
+
     this.radius = radius;
+
+    let defaultLocation ={ x:0 ,y:0};
+
+   let computeOptimumLocation= function(factor){
+        //...
+    }
     this.draw = function(){
+        let x,y;
+        computeOptimumLocation(0.1);
         console.log('draw');
     }
 }
 
 const circle = new Circle(10);
 
-//printing the property of object
-for (let key in circle){
-    console.log(key);
-}
-
-//printing the property of object with value
-for (let key in circle){
-    console.log(key,circle[key]);
-}
-
-//printing the property of object with value using filtering
-for (let key in circle){
-    if(typeof circle[key]!== 'function'){
-        console.log(key,circle[key]);
-    } 
-}
-
-//printing the property of object with value using array format
-const keys = Object.keys(circle);
-console.log(keys);
-
-//checking anny property exists on a object or not
-if('radius' in circle){
-    console.log('circle has radius');
-}
