b4b91bd235dbe64ee417acfe90cb43569be21ed8
Maharoz <mahruj66@gmail.com>
2018-12-13 19:35:22 +0600

    getter and setter method implemented

--- index.js
+++ index.js
@@ -1,20 +1,34 @@
-function Circle(radius){
-
-    let color = 'red'; //local variable
-
+function Circle(radius){ 
     this.radius = radius;
 
-    let defaultLocation ={ x:0 ,y:0};
+    let defaultLocation ={ x:5 ,y:6};
 
    let computeOptimumLocation= function(factor){
         //...
     }
+    this.getDefaultLocation = function(){
+        return defaultLocation;
+    }
     this.draw = function(){
-        let x,y;
-        computeOptimumLocation(0.1);
         console.log('draw');
     }
+
+    Object.defineProperty(this, 'defaultLocation',{
+        get: function(){
+            return defaultLocation;
+        },
+        set: function(value){
+            if(!value.x || !value.y){
+                throw new Error('Invalid location.')
+
+                defaultLocation = value;
+            }
+        }
+    })
 }
 
 const circle = new Circle(10);
+circle.defaultLocation =1;
+circle.getDefaultLocation();
+circle.draw();
 
