326cf8cf1956eb144150695b2770c5a4eead2b1d
maharoz <mahruj66@gmail.com>
2019-03-08 17:57:42 +0600

    primitive type implemented

---
+++ Type/index.js
@@ -0,0 +1,14 @@
+var customerName = "John";
+var accountBalance = 20000;
+var isValid = true;
+
+var object = null;
+var x = undefined;
+var y;
+
+document.write(customerName + "<br/>")
+document.write(accountBalance + "<br/>")
+document.write(isValid + "<br/>")
+document.write(object + "<br/>")
+document.write(x + "<br/>")
+document.write(y + "<br/>")
