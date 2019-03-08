9973bf0175795ab3268884b62f490e72aea94d52
maharoz <mahruj66@gmail.com>
2019-03-08 19:09:04 +0600

    use of some methods

--- Type/index.js
+++ Type/index.js
@@ -7,12 +7,17 @@ var x = 123;
 var y=x;
 y=465;
 var s = "123";
+var s1 ="You are the creator of your own destiny"
 
+document.write(s1.toLowerCase()+ "<br/>")
+document.write(s1.substring(5,10)+ "<br/>")
+document.write(s1.charAt(5)+ "<br/>")
+document.write(typeof x.toString() + "<br/>")
 document.write(typeof customerName + "<br/>")
 document.write(typeof accountBalance + "<br/>")
 document.write(typeof isValid + "<br/>")
 document.write((object === null) + "<br/>")
 document.write(typeof x + "<br/>")
 document.write(y + "<br/>")
-document.write(x==s);//true
-document.write(x===s);//false
+document.write((x==s)+ "<br/>");//true
+document.write((x===s)+ "<br/>");//false
