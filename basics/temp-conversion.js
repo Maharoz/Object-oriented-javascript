8e195bbc108c6767f801177213b881cc3d987426
maharoz <mahruj66@gmail.com>
2019-05-19 17:44:17 +0600

    Building a temparature converter

---
+++ basics/temp-conversion.js
@@ -0,0 +1,13 @@
+let farenheit = 50;
+
+//calculate celcius and store in calcius variable
+//print the value
+
+let celcius;
+ celcius= (farenheit-32)*(5/9);
+ console.log(celcius);
+//calculate kelvin and store in kelvin variable
+//print the value
+let kelvin;
+ kelvin = (farenheit+459.67)*(5/9);
+ console.log(kelvin);
