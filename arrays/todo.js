64b06a6e94159e37d356a6ea6d36cb439344f5c9
maharoz <mahruj66@gmail.com>
2019-05-25 10:36:41 +0600

    Array basics

---
+++ arrays/todo.js
@@ -0,0 +1,5 @@
+const todos =['Order cat Food','Clean Kitchen','buy food','don work','exercise'];
+
+ console.log(`You have ${todos.length} todos!`)
+console.log(`Todo: ${todos[0]}`)
+console.log(`Todo: ${todos[todos.length - 2]}`)
