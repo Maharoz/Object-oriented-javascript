535c276a54b76b57d3089e7958a6d72e55badca9
maharoz <mahruj66@gmail.com>
2019-06-02 08:51:19 +0600

    Manipulating array with method

--- arrays/note.js
+++ arrays/note.js
@@ -1,5 +1,10 @@
 const notes = ['Note 1', 'Note 2' , 'Note 3']
 
+notes.pop()
+notes.push('My New Notes')
+notes.shift()
+notes.unshift('My first note')
+notes.splice(1,1)
 console.log(notes.length);
 
-console.log(notes[22]);
+console.log(notes);
--- arrays/todo.js
+++ arrays/todo.js
@@ -1,6 +1,10 @@
 const todos =['Order cat Food','Clean Kitchen','buy food','don work','exercise'];
 
- console.log(`You have ${todos.length} todos!`)
-console.log(`Todo: ${todos[0]}`)
 
-console.log(`Todo: ${todos[todos.length - 2]}`)
+todos.splice(2,1)
+todos.push('Buy Coffe')
+todos.shift()
+//  console.log(`You have ${todos.length} todos!`)
+// console.log(`Todo: ${todos[0]}`)
+
+// console.log(`Todo: ${todos[todos.length - 2]}`)
